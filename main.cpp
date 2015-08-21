22af3c62682ae8f170afc9ac676dd8d848fc5bcf
Dongcy2015 <DONGCY@grandsoft.com.cn>
2015-08-21 20:29:32 +0800

    second

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[])
+int main(int argcm, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    QGuiApplication app(argcm, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    QQmlApplicationEngine *engine;
+    engine->load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
 }
